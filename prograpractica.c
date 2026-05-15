2148ecd5339a316594dbc782d51ae67ab718f602
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-15 14:28:18 -0500

    Navegación por el vector con punteros -Mateo Mero

--- prograpractica.c
+++ prograpractica.c
@@ -4,11 +4,11 @@ int main (int argc, char *argv[]) {
     int arreglo[]={3,6,9,12,15,18};
     int *ptr;
     ptr = &arreglo[0];          //ptr = arreglo;
-    printf("%p %d\t", ptr, *ptr);
 
     for (int i = 0; i < 6; i++)
     {
-        printf("%d ", arreglo[i]);
+        printf("arreglo[%d] = %d ", i, arreglo[i]);
+        printf("%d\n", *(ptr+i));
     }
 
     return 0;
