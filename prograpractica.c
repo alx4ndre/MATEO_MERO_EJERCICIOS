c458ab2e96404aa51c0634febe5c9785de0e913d
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-15 14:06:41 -0500

    Primer arreglo en C -Mateo Mero

---
+++ prograpractica.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    int arreglo[]={3,6,9,12,15,18};
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arreglo[i]);
+    }
+
+    return 0;
+}
