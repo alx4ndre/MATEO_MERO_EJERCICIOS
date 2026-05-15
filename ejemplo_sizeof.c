441f90d0aa1a88f3e2fd866ba4161a48086ea2b8
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-15 14:47:30 -0500

    Ejemplo 2 segundo commit -Mateo Mero

--- ejemplo_sizeof.c
+++ ejemplo_sizeof.c
@@ -2,6 +2,10 @@
 int main(int argc, char const *argv[])
 {
     int i = sizeof(int);
-    printf("%d byte(s)", i);
+    printf("%d byte(s)\n",i);
+
+    int arreglo[]={3,6,9,12,15,18};
+    int j = sizeof arreglo;
+    printf("%d byte(s)",j);
     return 0;
 }
