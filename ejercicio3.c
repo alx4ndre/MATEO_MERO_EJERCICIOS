227194eb5f7a58905e0ea6acad7eb1ced8ac2894
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-15 15:17:44 -0500

    Ejercicio 3 primer commit -Mateo Mero

---
+++ ejercicio3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    char vocales[]={'a','e','i','o','u'};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%c ", vocales[i]);
+    }
+
+    return 0;
+}
