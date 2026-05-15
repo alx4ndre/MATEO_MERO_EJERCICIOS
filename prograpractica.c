6c34640b44f23370a3cca4510471c92167420427
alx4ndre <mateo.mero@udla.edu.ec>
2026-05-15 14:21:05 -0500

    Implementación de punteros -Mateo Mero

--- prograpractica.c
+++ prograpractica.c
@@ -2,6 +2,10 @@
 
 int main (int argc, char *argv[]) {
     int arreglo[]={3,6,9,12,15,18};
+    int *ptr;
+    ptr = &arreglo[0];          //ptr = arreglo;
+    printf("%p %d\t", ptr, *ptr);
+
     for (int i = 0; i < 6; i++)
     {
         printf("%d ", arreglo[i]);
